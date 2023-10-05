dd334be97ef727fccef04bc0ed2247eacb8d136c
Priti-n-b <priti.bobde@codedchalk.com>
2023-10-05 11:29:42 +0530

    bugs corrected

--- src/app/services/case-studies-data.ts
+++ src/app/services/case-studies-data.ts
@@ -59,4 +59,145 @@ export const caseStudiesData = [
         },
 
   },
+
+  {
+    id: '002',
+    heading:"1. TRANSFORMING DESIGN AND PRINTING WITH INNOVATIVE WEB APPLICATION",
+    image:"../../../../assets/img/projects/2.jpg",
+    overview:[
+      {
+        title:"Background:",
+        description:["The client is a dynamic shop specializing in custom designs and printing services, including logos, emblems, graphics, and various product prints. Despite their creative expertise, the traditional design process using CorelDRAW was time-consuming and labor-intensive. To enhance their operations and deliver a superior customer experience, they sought a solution to streamline design and printing.",]
+    },
+      {
+    title:"Challenge",
+    description:["● Lengthy and complex design processes.","● Limited customization options for customers.","●Inability to offer design previews before printing.","● The demand for an efcient system to manage design orders."],
+
+},
+  {
+    title:"Wizgle Solution:",
+    description:["Wizgle collaborated with the Client to develop a groundbreaking web application that would revolutionize their design and printing workfows. Key components of the solution included:", " User-Centric Interface: The web application offered an intuitive and user-friendly interface. Customers could effortlessly upload product images, utilize an integrated design plugin, and preview their designs before fnalizing orders.","Design Template Library: A rich library of design templates was seamlessly integrated into the application, enabling customers to choose pre-made designs or customize them to their liking. This innovation substantially reduced design creation time.", "Real-Time Design Preview: Customers gained the ability to preview their designs in real-time, allowing for adjustments and fne-tuning before confrming orders. This feature greatly heightened customer satisfaction.", "Automated Printing Integration: The application was integrated with the printing equipment, automating the printing process and diminishing the need for manual intervention." ]
+
+  }
+],
+    solutions: [
+        {
+            title: "",
+            image: "",
+            details: "",
+        },
+
+    ],
+    impact:[
+        {
+          title: "Results:",
+          image: "../../../../assets/img/projects/2.jpg",
+          details:["The implementation of the web application delivered transformative outcomes for the Client.", "Operational Efciency: The design process was signifcantly expedited, reducing the time and effort required for custom designs.", "Expanded Product Range: Offering a wider array of customizable products beyond t-shirts broadened the customer base and amplifed sales.", "Elevated Customer Satisfaction: Design previews prior to printing ensured customers could visualize their orders accurately, resulting in heightened satisfaction and fewer revisions.","Streamlined Operations: Automation of the printing process led to decreased manual labor and increased overall productivity."],
+        },
+    ],
+    conclusion:{
+      cTitle:"Conclusion:",
+      cDescription:"Through their collaboration with Wizgle, the Client successfully addressed their design andprinting challenges, embracing innovation to enhance both operational efciency and customer satisfaction. The web application not only optimized their processes but also positioned the Client as a leader in the custom design and printing industry, poised for continued growth."},
+      additionalInfo:[
+        {
+          icon:"../../../../assets/img/icons/md/1.svg",
+          eTitle:"Data Engineering",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          icon:"../../../../assets/img/icons/md/2.svg",
+          eTitle:"Channel Platforms",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          icon:"../../../../assets/img/icons/md/3.svg",
+          eTitle:"Development",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        }
+      ],
+
+    projectInfo:
+        {
+          client: 'eCoshop Club',
+          service: 'App Development',
+          industry: 'Retail, eCommerce',
+          stack: 'Android, Realm, Dagger 2, RxJava'
+        },
+
+  },
+
+  {
+    id: '003',
+    heading:"2. HOLIDAYS AND TRAVEL - COMPLETE DIGITAL SOLUTION FOR TRAVEL AGENCY",
+    image:"../../../../assets/img/projects/2.jpg",
+    overview:[
+      {
+        title:"Client Requirments:",
+        description:["The client is deeply entrenched in the travel management business, specializing in travel bookings, hotel reservations, and crafting memorable holiday experiences. Their multifaceted requirements revolved around creating two distinct portals:",]
+    },
+      {
+    title:"Dynamic Holiday Packages Website:",
+    description:["Showcase a wide array of holiday packages from diverse destinations. Enable users to explore, select, and customize their itineraries. Allow users to share travel blogs, write reviews, and showcase their travel experiences through photographs. Serve as an invaluable travel guide with comprehensive information on global tourist destinations."],
+  },
+  {
+    title:"Content Management System (CMS):",
+    description:["Empower the client's back-end and management team to efciently manage bookings. Facilitate content addition and maintenance with a high degree of customization."],
+  },
+
+  {
+    title:"Wizgle Solution:",
+    description:["Wizgle devised an all-encompassing digital solution that catered to clients' needs:","Mobile-Responsive Holiday Website: Designed a visually appealing, fully mobile-responsive website.",
+    "Showcased a vast array of travel destinations and the agency's offerings.", "Provided a user-friendly platform for travelers to explore, personalize, and document their journeys.",
+    "Incorporates a robust knowledge base, serving as a comprehensive travel guide.",
+    "Flexible Content Management System (CMS):","Developed an intuitive CMS that granted the agency's management team control over the website.","Enabled dynamic content creation with complete customization options for aesthetics."]
+
+  }
+],
+    solutions: [
+        {
+            title: "",
+            image: "",
+            details: "",
+        },
+
+    ],
+    impact:[
+        {
+          title: "Results:",
+          image: "../../../../assets/img/projects/2.jpg",
+          details:["The implementation of Wizgle's digital solution yielded signifcant outcomes:", "Enhanced Customer Experience: Travelers enjoyed a seamless, visually engaging platform for exploring, customizing, and sharing their travel experiences.", "Efficient Operations: The CMS streamlined back-end operations, making bookings and content management more efficient..", "Increased User Engagement: User-generated content, including blogs and reviews, boosted user engagement and trust.", "Comprehensive Travel Guide: The portal's knowledge base became a valuable resource for travelers seeking information on global destinations.",
+        ],
+        },
+    ],
+    conclusion:{
+      cTitle:"Conclusion:",
+      cDescription:"With Wizgle's transformative digital solution, Client revolutionized its operations and enhanced customer experiences. The mobile-responsive website and user-friendly CMS positioned the agency as a forward-thinking industry leader, ready to meet the evolving needs of travelers. This digital transformation has set the stage for the Client to continue its success in the dynamic world of travel management."
+},
+      additionalInfo:[
+        {
+          icon:"../../../../assets/img/icons/md/1.svg",
+          eTitle:"Data Engineering",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          icon:"../../../../assets/img/icons/md/2.svg",
+          eTitle:"Channel Platforms",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        },
+        {
+          icon:"../../../../assets/img/icons/md/3.svg",
+          eTitle:"Development",
+          eDescription:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+        }
+      ],
+
+    projectInfo:
+        {
+          client: 'eCoshop Club',
+          service: 'App Development',
+          industry: 'Retail, eCommerce',
+          stack: 'Android, Realm, Dagger 2, RxJava'
+        },
+
+  },
 ]
